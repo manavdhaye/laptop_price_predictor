bad4d54d66f2a75979c4a6e627e380c81ce20858
manavdhaye <manavdhaye@gmail.com>
2025-07-22 16:25:33 +0530

    first commit

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import math
 import pandas as pd
 df=pickle.load(open("df.pkl","rb"))
 pipe = pickle.load(open("pipe.pkl", "rb"))
-st.title("Book Recommneder System")
+st.title("laptop Recommneder System")
 
 
 company = st.selectbox(("select company"),df["Company"].unique())
